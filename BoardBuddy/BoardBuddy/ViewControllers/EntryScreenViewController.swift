b7be93bb54f916fdaa2698ca67ad9c7385c68a0d
Jamneeley <jamneeley@gmail.com>
2018-05-15 16:13:40 -0600

    added storyboard refs and VC for entry - homescreen

---
+++ BoardBuddy/BoardBuddy/ViewControllers/EntryScreenViewController.swift
@@ -0,0 +1,43 @@
+//
+//  EntryScreenViewController.swift
+//  BoardBuddy
+//
+//  Created by James Neeley on 5/15/18.
+//  Copyright © 2018 Francisco Diarte. All rights reserved.
+//
+
+import UIKit
+
+class EntryScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func createButtonTapped(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func joinButtonTapped(_ sender: Any) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
