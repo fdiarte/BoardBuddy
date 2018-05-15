ba92ae6be49f45e2adcca270f950e3cce02158d9
Francisco Diarte <fdiarte95@gmail.com>
2018-05-15 16:56:07 -0600

    add lobby functionality

--- BoardBuddy/BoardBuddy/ViewControllers/EntryScreenViewController.swift
+++ BoardBuddy/BoardBuddy/ViewControllers/EntryScreenViewController.swift
@@ -7,37 +7,39 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
 class EntryScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
     @IBAction func createButtonTapped(_ sender: Any) {
-        
-        
+        MPCManager.shared.advertiserAssistant.start()
+        // Segue to lobby
+        let storyboard = UIStoryboard(name: "Lobby", bundle: nil)
+        let view = storyboard.instantiateViewController(withIdentifier: "lobby")
+        present(view, animated: true, completion: nil)
     }
     
     @IBAction func joinButtonTapped(_ sender: Any) {
-        
+        MPCManager.shared.browser.delegate = self
+        present(MPCManager.shared.browser, animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension EntryScreenViewController: MCBrowserViewControllerDelegate {
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+        let storyboard = UIStoryboard(name: "Lobby", bundle: nil)
+        let view = storyboard.instantiateViewController(withIdentifier: "lobby")
+        present(view, animated: true, completion: nil)
     }
-    */
-
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+        // Segue to lobby
+    }
 }
