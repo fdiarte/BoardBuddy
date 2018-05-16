7bd0d1ffe936b414a2ffbcc6a9e36fd25eb55218
Jamneeley <jamneeley@gmail.com>
2018-05-16 11:41:35 -0600

    created and wired up the createsessionVC

--- BoardBuddy/BoardBuddy/ViewControllers/CreateSessionViewController.swift
+++ BoardBuddy/BoardBuddy/ViewControllers/CreateSessionViewController.swift
@@ -8,28 +8,136 @@
 
 import UIKit
 
-class CreateSessionViewController: UIViewController {
-
+class CreateSessionViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: - Properties
+    
+    @IBOutlet weak var sessionNameTextField: UITextField!
+    @IBOutlet weak var moneyPerPlayerTextField: UITextField!
+    @IBOutlet weak var playerNameTextField: UITextField!
+    
+    @IBOutlet weak var bootButton: UIButton!
+    @IBOutlet weak var cannonButton: UIButton!
+    @IBOutlet weak var dogButton: UIButton!
+    @IBOutlet weak var moneyButton: UIButton!
+    @IBOutlet weak var carButton: UIButton!
+    @IBOutlet weak var thimbleButton: UIButton!
+    @IBOutlet weak var wheelbarrowButton: UIButton!
+    @IBOutlet weak var hatButton: UIButton!
+    let singleTap = UITapGestureRecognizer()
+    var playerImage: UIImage?
+    
+    //MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setupObjects()
+        sessionNameTextField.delegate = self
+        moneyPerPlayerTextField.delegate = self
+        playerNameTextField.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: - Methods
+    
+    func setupObjects() {
+        singleTap.addTarget(self, action: #selector(disableKeyboard))
+        sessionNameTextField.returnKeyType = .done
+        moneyPerPlayerTextField.keyboardType = .numberPad
+        moneyPerPlayerTextField.returnKeyType = .done
+        playerNameTextField.returnKeyType = .done
     }
-    */
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
+    }
+    
+    @objc func keyBoardWillShow() {
+        view.addGestureRecognizer(singleTap)
+    }
+    
+    @objc func keyBoardWillHide() {
+        view.removeGestureRecognizer(singleTap)
+    }
+    
+    @objc func disableKeyboard() {
+        view.endEditing(true)
+    }
+    
+    //MARK - ButtonMethods
+    
+    @IBAction func bootButtonTapped(_ sender: Any) {
+        print("boot")
+        highlight(Button: bootButton)
+        playerImage = #imageLiteral(resourceName: "BootIcon")
+    }
+    @IBAction func cannonButtonTapped(_ sender: Any) {
+        print("cannon")
+        highlight(Button: cannonButton)
+        playerImage = #imageLiteral(resourceName: "CannonIcon")
+    }
+    @IBAction func dogButtonTapped(_ sender: Any) {
+        print("dog")
+        highlight(Button: dogButton)
+        playerImage = #imageLiteral(resourceName: "DogIcon")
+    }
+    @IBAction func moneyButtonTapped(_ sender: Any) {
+        print("money")
+        highlight(Button: moneyButton)
+        playerImage = #imageLiteral(resourceName: "MoneyBagIcon")
+    }
+    @IBAction func carButtonTapped(_ sender: Any) {
+        print("car")
+        highlight(Button: carButton)
+        playerImage = #imageLiteral(resourceName: "RaceCar")
+    }
+    @IBAction func thimbleButtonTapped(_ sender: Any) {
+        print("thimble")
+        highlight(Button: thimbleButton)
+        playerImage = #imageLiteral(resourceName: "ThimbleIcon")
+    }
+    
+    @IBAction func wheelbarrowButtonTapped(_ sender: Any) {
+        print("wheelbarrow")
+        highlight(Button: wheelbarrowButton)
+        playerImage = #imageLiteral(resourceName: "Wheelbarrow")
+    }
+    @IBAction func hatButtonTapped(_ sender: Any) {
+        print("hat")
+        highlight(Button: hatButton)
+        playerImage = #imageLiteral(resourceName: "TophatIcon ")
+    }
+    
+    func highlight(Button button: UIButton) {
+        bootButton.tintColor = Colors.mintCreme
+        cannonButton.tintColor = Colors.mintCreme
+        dogButton.tintColor = Colors.mintCreme
+        moneyButton.tintColor = Colors.mintCreme
+        carButton.tintColor = Colors.mintCreme
+        thimbleButton.tintColor = Colors.mintCreme
+        wheelbarrowButton.tintColor = Colors.mintCreme
+        hatButton.tintColor = Colors.mintCreme
+        button.tintColor = Colors.green
+    }
 
+    @IBAction func createSessionButtonTapped(_ sender: Any) {
+        guard let sessionName = sessionNameTextField.text, !sessionName.isEmpty, let moneyAmount = moneyPerPlayerTextField.text, !moneyAmount.isEmpty, let image = playerImage else { presentAlert(); return}
+        //create session
+        
+        //create player
+        
+        //push Lobby
+        let storyboard = UIStoryboard(name: "Lobby", bundle: nil)
+        let view = storyboard.instantiateViewController(withIdentifier: "lobby")
+        present(view, animated: true, completion: nil)
+    }
+    
+    func presentAlert() {
+        let alert = UIAlertController(title: "Uh Oh", message: "Make sure all fields have a value", preferredStyle: .alert)
+        let dismiss = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+        alert.addAction(dismiss)
+        present(alert, animated: true, completion: nil)
+    }
 }
