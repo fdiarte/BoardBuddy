9fff4b4e35c566235933e7b0bb39487189481b00
Hayden Murdock <haydenmurdock@Haydens-MacBook-Air.local>
2018-05-16 12:04:41 -0600

    updated rules page

---
+++ BoardBuddy/BoardBuddy/ViewControllers/RulesCell.swift
@@ -0,0 +1,16 @@
+//
+//  RulesCell.swift
+//  BoardBuddy
+//
+//  Created by Hayden Murdock on 5/16/18.
+//  Copyright © 2018 Francisco Diarte. All rights reserved.
+//
+
+import UIKit
+
+class RulesCell: UITableViewCell {
+
+    @IBOutlet weak var rulesLabel: UILabel!
+    
+
+}
