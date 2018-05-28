986300a4642d19e86a5984015c1630b998a42a5d
Jamneeley <jamneeley@gmail.com>
2018-05-28 12:28:28 -0600

    added second launch screen, instructionsPageViewController, NSUserDefaults for isCurrentUSer

---
+++ BoardBuddy/BoardBuddy/Models/ViewControllers/InstructionPages/instruction3ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  instruction3ViewController.swift
+//  BoardBuddy
+//
+//  Created by James Neeley on 5/28/18.
+//  Copyright © 2018 Francisco Diarte. All rights reserved.
+//
+
+import UIKit
+
+class Instruction3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
